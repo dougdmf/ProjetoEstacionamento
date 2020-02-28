77f78df34ad7acb93e5ee5cc32dd55089846fbc7
dougdmf <61568001+dougdmf@users.noreply.github.com>
2020-02-27 22:06:34 -0300

    upload da 1a versao class java aluno
    upload da 1a versao class java aluno

---
+++ Fontes/Aluno.java
@@ -0,0 +1,18 @@
+package teste;
+
+public class Aluno {
+
+	int rm;
+	String nome;
+	
+	public static void main(String[] args) {
+		
+		
+		Aluno a = new Aluno();
+		System.out.println(a);
+		System.out.println(a.rm);
+		System.out.println(a.nome);
+
+	}
+
+}
